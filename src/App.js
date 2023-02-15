8512f9541c5cdc369f9e825a4c601298412950a6
Roshni <roshnimprasad@gmail.com>
2023-02-15 17:11:46 +0530

    updated background features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 // import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header.js';
+import Home from './screens/Home.js';
+import About from './screens/About.js';
 
 const App = () => {
   return (
     <div className="App_Outer">
-      <div className="App_Inner">My name is Roshni Prasad. </div>
-      <div className="App_Inner">I study at MITS</div>
-      <div className="App_Inner">I like to code.</div>
-   
-      {/* <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="about" element={<About />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="resume" element={<Resume />} />
-        <Route path="articles" element={<Articles />} />
-      </Routes> */}
+      <div className="App_Inner1">Roshni Prasad </div>
+      <div className="App_Inner2">Energetic and passionate 21 years old college student, currently pursuing computer science and engineering in Muthoot Institute Of Technology and Science, who wish to see herself as a software engineer.</div>
+
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
 
     </div>
   )
 }
 
+
 export default App;
+
