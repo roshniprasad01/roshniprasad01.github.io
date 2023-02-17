640bece36b47f97b97e707866cefa28dae4ec259
Roshni <roshnimprasad@gmail.com>
2023-02-17 17:03:06 +0530

    updated css

--- src/App.js
+++ src/App.js
@@ -16,13 +16,17 @@ const App = () => {
       <img src="https://avatars.githubusercontent.com/u/84218269?v=4" width="200px" align="left" border="3"></img>
 
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route exact path="/Projects" element={<Projects />} />
-          <Route path="/Articles" element={<Articles />} />
-        </Routes>
+        <div className="App_Inner2">
+          <div className="App_Inner3">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route exact path="/Projects" element={<Projects />} />
+              <Route path="/Articles" element={<Articles />} />
+            </Routes>
+          </div>
+          <Header />
+        </div>
       </BrowserRouter>
 
     </div>
