4f40513ff800e127ee3f9cd988e6e19917f31588
Roshni <roshnimprasad@gmail.com>
2023-02-17 23:09:20 +0530

    formt

--- src/screens/About.js
+++ src/screens/About.js
@@ -2,14 +2,14 @@
 import '../css/About.css';
 
 const About = () => {
-    return (
-      <div className="about">
-        <h1 className='about1'>About</h1>
-        <form>
+  return (
+    <div className="about">
+      <h1 className='about1'>About</h1>
+      <div>
         <p>Energetic and passionate 21 years old college student, currently pursuing computer science and engineering in Muthoot Institute Of Technology and Science, who wish to see herself as a software engineer.</p>
-        </form>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default About;
+export default About;
